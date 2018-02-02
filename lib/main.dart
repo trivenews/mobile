d4031c0c49641f79c0b6f36df484f6b4458e623f
kabiir <Sid976628@gmail.com>
2018-02-02 22:38:31 +0530

    add screen shot preview screen
    Restructure project files
    Add routing mechanism
    Update pubspec.yaml
    
    simplified build.gradle to work with debug key provided by flutter in case release key is not present

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,28 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
-
-class TriveApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Trive',
-      theme: new ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.grey,
-        accentColor: Colors.orangeAccent[700],
-      ),
-      home: new MyHomePage(title: 'Trive'),
-    );
-  }
-
-}
-
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-
-class _MyHomePageState extends State<MyHomePage> {
-  File imageFile;
-
-  getImage() async {
-    var _fileName = await ImagePicker.pickImage();
-    setState(() {
-      imageFile = _fileName;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Center(
-              child: imageFile == null
-                  ? new Text('No image selected.')
-                  : new Image.file(imageFile),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: new Icon(Icons.add_a_photo),
-      ),
-    );
-  }
-}
-
+import 'routes.dart';
 
 void main() {
   runApp(new TriveApp());
+}
+
+class TriveApp extends StatefulWidget {
+  @override
+  _TriveAppState createState() => new _TriveAppState();
+}
+
+class _TriveAppState extends State<TriveApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.grey,
+          accentColor: Colors.orangeAccent[700]),
+      home: appRoutes[Screen.MainScreen](),
+
+      /// App title, this is different from a screen title.
+      title: "Trive",
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
