289c4251e78f64b1afd9c98cf884d3e70bb356ed
kabiir <Sid976628@gmail.com>
2018-02-03 22:55:37 +0530

    Restructure project files Add routing mechanism Update pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,28 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-// import 'package:fluro/fluro.dart';
-import 'screenshot_preview_screen.dart';
+import 'routes.dart';
 
-File imageFile;
 void main() {
-  /// App routes
-  final Map<String, WidgetBuilder> appRoutes = <String, WidgetBuilder>{
-    MainScreen.routeName: (BuildContext context) => new MainScreen(
-          key: new GlobalKey<ScaffoldState>(debugLabel: 'MainScreen'),
-          title: 'Trive',
-        ),
-    ScreenShotPreview.routeName: (BuildContext context) =>
-        new ScreenShotPreview(imageFile),
-  };
-
-  // final router  = new Router();
-  runApp(new MaterialApp(
-    theme: new ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.grey,
-        accentColor: Colors.orangeAccent[700]),
-    routes: appRoutes,
-
-    /// App title, this is different from a screen title.
-    title: "Trive",
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(new TriveApp());
 }
 
-class MainScreen extends StatefulWidget {
-  static String routeName = '/';
-  MainScreen({Key key, this.title}) : super(key: key);
-  final String title;
+class TriveApp extends StatefulWidget {
   @override
-  _MainScreenState createState() => new _MainScreenState();
+  _TriveAppState createState() => new _TriveAppState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  getImage() async {
-    var _fileName = await ImagePicker.pickImage();
-    setState(() {
-      imageFile = _fileName;
-    });
-  }
-
+class _TriveAppState extends State<TriveApp> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Center(
-              child: imageFile == null
-                  ? new Text('No image selected.')
-                  : new FlatButton(
-                      child: new Text('Proceed'),
-                      onPressed: textF,
-                      color: Colors.green,
-                    ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: new Icon(Icons.add_a_photo),
-      ),
-    );
-  }
+    return new MaterialApp(
+      theme: new ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.grey,
+          accentColor: Colors.orangeAccent[700]),
+      home: appRoutes[Screen.MainScreen](),
 
-  textF() {
-    Navigator.pushNamed(context, '/screenshots/preview');
+      /// App title, this is different from a screen title.
+      title: "Trive",
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
--- lib/screens/screenshot_preview_screen.dart
+++ lib/screens/screenshot_preview_screen.dart
@@ -1,14 +1,11 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'dart:io';
 
 class ScreenShotPreview extends StatefulWidget {
   static const String routeName = '/screenshots/preview';
   final File file;
-  ScreenShotPreview(this.file);
+  ScreenShotPreview([Map<String, dynamic> params = const {}])
+      : file = params['file']?.runtimeType == File ? params['file'] : null;
 
   @override
   ScreenShotPreviewState createState() => new ScreenShotPreviewState(file);
@@ -18,7 +15,7 @@ enum AppBarBehavior { normal, pinned, floating, snapping }
 
 class ScreenShotPreviewState extends State<ScreenShotPreview> {
   static final GlobalKey<ScaffoldState> _scaffoldKey =
-      new GlobalKey<ScaffoldState>(debugLabel: 'ScreenShotPreview');
+      new Key('ScreenShotsPreview');
   final double _appBarHeight = 256.0;
   File file;
   ScreenShotPreviewState(this.file);
