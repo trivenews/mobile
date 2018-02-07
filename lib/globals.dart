059ad904fdb641eb8e79552e14d58fd54c245410
kabiir <Sid976628@gmail.com>
2018-02-07 23:31:53 +0530

    add helper function to test runtimteType mismatch and null values

--- lib/globals.dart
+++ lib/globals.dart
@@ -13,3 +13,18 @@ class Globals {
   static TriveImagePicker imagePicker = new TriveImagePicker();
   static File selectedImageFile;
 }
+
+/// Checks if runtimeType match for [value] and [variable],
+/// if [allowNull] is `false` returns [value] if it's not null,
+/// throws `ArgumentError.notNull` otherwise.
+/// Specify [varName] for debugging.
+dynamic GetVal(dynamic value, dynamic variable,
+    {String varName, bool allowNull = false}) {
+  if (value.runtimeType == variable.runtimeType) {
+    if (!allowNull && (null == value)) {
+      throw new ArgumentError.notNull('${varName ?? ''}');
+    }
+    return value;
+  }
+  throw new ArgumentError('${varName ?? ''} type mismatch');
+}
